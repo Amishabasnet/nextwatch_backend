328562c967aacda0bfbb7a8ae8da95c2f4916dd1
Amisha Basnet <amishaaaaaa045@gmail.com>
2026-06-13 23:28:17 +0545

    serrverjs updated

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ const movieRoutes = require('./routes/movieRoutes');
 const featureRoutes = require('./routes/featureRoutes');
 const historyRoutes = require('./routes/historyRoutes');
 const ratingRoutes = require('./routes/ratingRoutes');
+const recommendationRoutes = require('./routes/recommendationRoutes');
 
 const app = express();
 
@@ -23,6 +24,8 @@ app.use('/api/movies', movieRoutes);
 app.use('/api/features', featureRoutes);
 app.use('/api/history', historyRoutes);
 app.use('/api/ratings', ratingRoutes);
+app.use('/api/recommendations', recommendationRoutes);
+
 
 // Health check
 app.get('/', (req, res) => {
